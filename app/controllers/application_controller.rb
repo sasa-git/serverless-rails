2370bbae032d5bd371377c1d646fffe617e6bda3
sasasoni <sasasoni1119@gmail.com>
2019-11-25 18:25:24 +0900

    CSRFの無効化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery
+  # or use this...
+  # protect_from_forgery with: :null_session
+  # ref... https://kanoe.studio/archives/666, https://qiita.com/kurashita/items/d1c8f6d79daec89c368c
+  # or use "protect_from_forgery unless: ->{ request.format.json? }" : it only json request
 end
